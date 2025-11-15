2b824bc5a73ff48bec1f47b7066b64488c14eb32
BearForOdin <selasidzeamesi82@gmail.com>
2025-11-15 11:56:09 -0700

    sccann

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year', 'author')
-    search_fields = ('title', 'author')
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Fields shown in admin list view
+    list_display = ("username", "email", "date_of_birth", "is_staff")
+
+    # Fields allowed when editing a user
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fields shown when creating a new user
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
