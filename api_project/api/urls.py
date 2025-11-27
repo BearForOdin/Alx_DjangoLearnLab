9c4d87236352466772529e29756c7f5780c72895
BearForOdin <selasidzeamesi82@gmail.com>
2025-11-27 11:03:54 -0700

    url

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookList, BookViewSet
 from rest_framework.authtoken.views import obtain_auth_token
+from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -11,4 +11,3 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
-
