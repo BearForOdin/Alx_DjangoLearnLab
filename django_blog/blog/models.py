f6749f78c56f8c74e749b8bcf0438c3c9a11b0d5
BearForOdin <selasidzeamesi82@gmail.com>
2025-12-05 20:24:37 -0700

    letstry

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,15 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-
-    def __str__(self):
-        return self.title
-    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
@@ -20,11 +11,14 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username} Profile"
 
-# auto-create / save Profile when User is created
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
     else:
-        instance.profile.save()
-
+        # Ensure profile exists and is saved when user is updated
+        Profile.objects.get_or_create(user=instance)
+        try:
+            instance.profile.save()
+        except Exception:
+            pass
