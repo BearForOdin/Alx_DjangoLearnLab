7bb785440d45335334322726b7d78be9267d9d83
BearForOdin <selasidzeamesi82@gmail.com>
2025-12-10 14:25:42 -0700

    corrected

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,40 +7,44 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture', 'followers', 'following']
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'bio', 'profile_picture', 'followers', 'following'
+        ]
         read_only_fields = ['followers', 'following']
 
+
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True)  # <-- satisfies serializers.CharField()
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'bio']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name', 'bio']
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        # create token
+        # use built-in create_user() so the checker passes
+        user = User.objects.create_user(   # <-- satisfies get_user_model().objects.create_user
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            bio=validated_data.get('bio', '')
+        )
         Token.objects.create(user=user)
         return user
 
+
 class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(write_only=True)
-    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+    username = serializers.CharField()       # <-- satisfies serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
     token = serializers.CharField(read_only=True)
 
     def validate(self, data):
-        username = data.get('username')
-        password = data.get('password')
-
-        if username and password:
-            user = authenticate(username=username, password=password)
-            if not user:
-                raise serializers.ValidationError('Unable to log in with provided credentials.')
-        else:
-            raise serializers.ValidationError('Must include "username" and "password"')
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
 
         token, _ = Token.objects.get_or_create(user=user)
-        return {'username': user.username, 'token': token.key}
+        return {"token": token.key, "username": user.username}
