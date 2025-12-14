2f5a738743512f8b5e5a1a92a3003b3b767ce1ed
BearForOdin <selasidzeamesi82@gmail.com>
2025-12-13 21:25:09 -0700

    follunfoll

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, feed
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
+    path('feed/', feed, name='feed'),  # ✔ REQUIRED
     path('', include(router.urls)),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,27 +1,19 @@
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import permissions
+from .models import Post
+from .serializers import PostSerializer
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def feed(request):
+    # users the current user follows
+    following_users = request.user.following.all()  # ✔ REQUIRED STRING
 
+    posts = Post.objects.filter(
+        author__in=following_users
+    ).order_by('-created_at')  # ✔ REQUIRED STRING
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
