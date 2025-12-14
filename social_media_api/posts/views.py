d4115d022f4cd180664e1a6e93fc5d698b60330a
BearForOdin <selasidzeamesi82@gmail.com>
2025-12-13 21:28:57 -0700

    move

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,19 +1,37 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
 from rest_framework.response import Response
-from rest_framework import permissions
-from .models import Post
-from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+from .serializers import PostSerializer, CommentSerializer
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated])
+@permission_classes([IsAuthenticated])
 def feed(request):
-    # users the current user follows
-    following_users = request.user.following.all()  # ✔ REQUIRED STRING
-
-    posts = Post.objects.filter(
-        author__in=following_users
-    ).order_by('-created_at')  # ✔ REQUIRED STRING
-
+    following_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
-
